16e74d695a197a9b41d53c33ab1a06f1c831cebf
Harshvardhan Gaggar <81851043+Arrow838@users.noreply.github.com>
2024-11-29 15:46:54 +0530

    Add files via upload

---
+++ code.c
@@ -0,0 +1,24 @@
+
+
+
+// Documentation
+
+
+
+// Libraries
+
+
+/*
+Main - open a file
+Each line in file has different issues with unique first number
+Send the line of all unique number to a function (Test)
+
+If test returns 0, it is secure else unsecure
+
+If it is unsecure, print the whole line which gives the error as well as the unique number code 
+
+*/
+
+
+
+

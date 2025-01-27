6ae635047f7d79a8a2fe19034cd94712260bd861
Harshvardhan Gaggar <81851043+its-harsh-here@users.noreply.github.com>
2025-01-27 17:48:17 +0530

    Update main.c

--- proj1/main.c
+++ proj1/main.c
@@ -1,49 +1,19 @@
 /*
-
-Summary: This code would do basic system checkup to see if system is secure or not. An input of user's IP address by 
-running the following command in your terminal: ./main <IP Address>
-
+Summary: This code checks scans the list of ports in "ports.txt" and returns the ports which are open. 
+An input of user's IP address by running the following command in your terminal: ./main <IP Address>
 
 Code was written and tested on: 
 - Operating System: Ubuntu 24.04.1 LTS                              
 - Kernel: Linux 6.8.0-49-generic
 - Architecture: x86-64
 
-
-This code checks the following loopholes: 
-
-0. Checks if unauthorized User is logged in using the command "w"
-    "w" returns information about users that are currently logged in so if there is another user logged in, we would get to 
-    know that the system is insure as they would be logged in by a different IP address. We use system(w) to run and print
-    the result of w in the terminal.
-
-1. Checks brodcast IP address using the command ifconfig
-    ifconfig returns the inet, netmask and broadcast IP address. This code checks for Broadcast IP address by using the 
-    strtok function to break the result into different lines, and strstr to check if IP address is there in the line which
-    has broadcast given.
-
-2. Checks if password less sudo is enabled which
-    "sudo" grants a user temporary privileges which can be used to carry out system administrative duties. Thus a device's
-    security would be heavily compromised if password less sudo is enabled. We used to command "sudo -n true" to check if
-    sudo requires a password or not. The command return "sudo: a password is required" if password is required, and exits
-    if passwordless sudo is enabled. 
-
-3. Checks if SSH root login is enabled
-    SSH(Secure Shell) is a protocol with which one can login to a device remotely. If root login is enabled, one can login
-    to the device remotely without the need of a password, so the device's security is heavily compromised. 
-    The command used is "grep "PermitRootLogin" /etc/ssh/sshd_config" which returns 
-    1. PermitRootLogin yes: Root login is enabled.
-    2. PermitRootLogin no: Root login is disabled.
-    3. PermitRootLogin prohibit-password: Root login is allowed only with key-based authentication.   
-
+Tools Used: 
+- Nmap
+- Terminal
 
 Future Add-ons:
-
-1. Check audit log of logged users for suspicious activities
-2. Check file permissions to see who all have permissions
-3. Check Firewall Configuration to find any security flaws
-4. Automate Monitoring and Reporting
-
+1. Write a menu driven code which gives the user a choice to get a description of the open port and how to close it. 
+2. TBD
 */
 
 #include <stdio.h>
@@ -53,16 +23,12 @@ Future Add-ons:
 #define SIZE 1000
 
 void runcmd(char *, char *, int);
-int uip(char*, char*);
-int pswsudo(char*);
-int rootlogin(char*);
-
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Invalid Input!\n");
+        printf("Invalid Input!\n Please try again with \"./main <IP Address>\"\n");
         return 1;
     }
     char *ip = argv[0]; // User IP
